7351ca689aeda335553eab22da9ffe748b47ba5c
Andrew Baughns <abaughns12@gmail.com>
2025-02-10 13:39:13 -0500

    Added help section to run_chat.sh and updated README.md

--- run_chat.sh
+++ run_chat.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Check for --help flag
+if [[ "$1" == "--help" ]]; then
+    cat help.txt
+    exit 0
+fi
+
+# Activate the virtual environment
 source venv/bin/activate
+
+# Run the chat runner script
 python chat_runner.py
